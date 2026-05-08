1cb767eec85ff6fbf525c29f242721768ff86855
Bekzod7788 <bek70507030@gmail.com>
2026-05-08 07:54:03 +0500

    fix vercel build warnings

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false,        // Turbopack o‘rniga Webpack ishlatamiz
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Brauzer tomonida keraksiz Node modullarini bo‘shatib qo‘yamiz
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-    return config;
-  },
+  productionBrowserSourceMaps: false,
 };
 
 module.exports = nextConfig;
